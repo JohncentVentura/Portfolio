bdf6b59f50da8ceddb8008e756993102cb2bcb3d
Johncent Ventura <johncent.developer@gmail.com>
2024-07-05 10:01:27 +0800

    Fix netlify refresh page not found 1.0

--- src/components/Components.jsx
+++ src/components/Components.jsx
@@ -216,9 +216,4 @@ export const IconDiv = ({ className, iconSFX, iconHref, icon, iconName }) => {
   );
 };
 
-export const ScrollToTop = () => {
-  const { pathname } = useLocation();
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
-};
+
